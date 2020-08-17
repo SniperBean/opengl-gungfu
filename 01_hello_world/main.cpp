ed90df9cdffac71e432f5fa064dea29b4d3a9473
SniperBean <fboy20045@gmail.com>
2020-08-17 23:56:28 +0800

    Update 01_hello_world

--- 01_hello_world/main.cpp
+++ 01_hello_world/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-// #include <GLFW/glfw3.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
@@ -11,24 +10,18 @@ void My_Display();
 int main(int argc, char* argv[]){
     glutInit(&argc, argv);
     cout << "hello! freeglut!";
-    #ifdef _MSC_VER
-        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-    #else
-        // glutInitDisplayMode(GLUT_3_2_CORE_PROFILE | GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-    #endif
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(600, 600);
     glutCreateWindow("hello glut!");
-    // glClearColor(backgroundGray, backgroundGray, backgroundGray, 1.0f);
     glutDisplayFunc(My_Display);
     glutMainLoop();
 }
 
 void My_Display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(0.0, 0.0, 0.0, 1.0f);
     glutSwapBuffers();
 }
 
